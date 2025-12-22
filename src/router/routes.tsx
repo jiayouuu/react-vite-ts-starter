f17ae1a8bec40f19f0f491c1efa8bcf1704bec28
桂佳囿 <3010336955@qq.com>
2025-12-22 10:30:24 +0800

    first commit

---
+++ src/router/routes.tsx
@@ -0,0 +1,31 @@
+/*
+ * @Author: 桂佳囿
+ * @Date: 2025-12-12 13:56:36
+ * @LastEditors: 桂佳囿
+ * @LastEditTime: 2025-12-22 09:41:49
+ * @Description: 路由配置
+ */
+import App from "@/App";
+import { lazyLoad } from "@/components/LazyLoad";
+import { type RouteObject } from "react-router-dom";
+
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "auth/login",
+        element: lazyLoad(() => import("@/views/auth/login")),
+      },
+      {
+        path: "auth/register",
+        element: lazyLoad(() => import("@/views/auth/register")),
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: lazyLoad(() => import("@/views/notFound")),
+  },
+];
